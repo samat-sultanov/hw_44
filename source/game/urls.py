eea94fca8bc5c6faa0851541c33c04641fed2030
samat_sultanov <samatsayak@gmail.com>
2022-11-01 17:52:57 +0600

    added webapp into settings; created style.css for our own styles; created input_form.html for user input; created game_page function in webapp.views to handle get request

--- source/game/urls.py
+++ source/game/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import game_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', game_page)
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def game_page(request):
+    if request.method == 'GET':
+        return render(request, 'input_form.html')

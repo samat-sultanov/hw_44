0e2bb50f23cb183a2787a46a81d521a19cf6806f
samat_sultanov <samatsayak@gmail.com>
2022-11-01 17:55:58 +0600

    created function generate_numbers to generate numbers, guess_numbers to handle user input numbers

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
+from random import sample
+
+
+def generate_numbers(num):
+    return sample(range(1, 10), 4)
+
+
+secret_nums = generate_numbers(4)
+game_history = []
 
 
 def game_page(request):
     if request.method == 'GET':
         return render(request, 'input_form.html')
+
+
+def guess_numbers(secret, actual):
+    global secret_nums
+    global game_history
+    if all(item.isdigit() for item in actual):
+        result = list(map(int, actual))
+        if len(set(result)) == len(result) and all(0 < x < 10 for x in result) and len(result) == 4:
+            bulls_cows = [0, 0]
+            for num1, num2 in zip(secret, result):
+                if num2 in secret:
+                    if num2 == num1:
+                        bulls_cows[0] += 1
+                    else:
+                        bulls_cows[1] += 1
+            if bulls_cows[0] == 4:
+                secret_nums = generate_numbers(4)
+                game_history = []
+                return "You got it right! New Game started!"
+            game_history.append(f"Bulls: {bulls_cows[0]}, Cows: {bulls_cows[1]}")
+            return f"Bulls: {bulls_cows[0]}, Cows: {bulls_cows[1]}"
+    return "A ValueError occurred! (Enter 4 unique numbers in range 1 and 9)"
